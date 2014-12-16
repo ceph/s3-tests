41e230be8ff0eba69303199ba588b999e266360c
Alfredo Deza <alfredo@deza.pe>
2014-12-16 09:28:15 -0500

    fix proper author of library
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version='0.0.1',
     packages=find_packages(),
 
-    author='Tommi Virtanen',
-    author_email='tommi.virtanen@dreamhost.com',
+    author='Red Hat Storage',
+    author_email='ceph-qa@ceph.com',
     description='Unofficial Amazon AWS S3 compatibility tests',
     license='MIT',
     keywords='s3 web testing',
