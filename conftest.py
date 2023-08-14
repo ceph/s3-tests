6200640192321a49d52b302cf370a7f2389a3601
Maksim Gelbakhiani <max@nspcc.ru>
2023-08-14 20:24:23 +0400

    Pytest and Allure support
    [ Fixes nspcc-dev/neofs-s3-gw#780 ]
    
    Signed-off-by: Maksim Gelbakhiani <max@nspcc.io>

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import configparser
+from s3tests_boto3.functional import setup
+
+def read_config(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config
+
+@pytest.fixture(scope='session', autouse=True)
+def nose_setup():
+    setup()
